b37ef98036a094e6489a2a236b2cbcd559810408
Nergisss <nergis.aktug2014@gmail.com>
2019-11-19 21:51:19 +0300

    otopark1

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace otopark.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
